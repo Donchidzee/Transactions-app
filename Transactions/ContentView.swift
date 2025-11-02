63ca528b9bcf7486b32a398e714eda25ee3967c8
Daniyar <donchik020202@gmail.com>
2025-11-02 13:12:23 +0100

    Initial Commit

---
+++ Transactions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Transactions
+//
+//  Created by Daniyar Yegeubay on 02/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Transactions/TransactionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionsApp.swift
+//  Transactions
+//
+//  Created by Daniyar Yegeubay on 02/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TransactionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
